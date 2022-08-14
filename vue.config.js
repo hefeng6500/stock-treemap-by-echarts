0328dc772e8108b72cbbad19aeb2559e657a8a59
hefeng6500 <2443992009@qq.com>
2022-08-14 17:29:45 +0800

    fix

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/stock-treemap-by-echarts/" : "/",
   transpileDependencies: true,
   lintOnSave: false, //关闭eslint检查
 });
